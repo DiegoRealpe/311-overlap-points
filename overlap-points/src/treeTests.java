17d332a8b886904d35a361495818c9783486fa02
diegort <diegort@iastate.edu>
2019-03-30 15:55:04 -0500

    Added checks in Node for if it is a Nil node! Added insert 1 test

--- overlap-points/src/treeTests.java
+++ overlap-points/src/treeTests.java
@@ -4,26 +4,53 @@ import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 public class treeTests {
-	
+
+	private static final int RED = 0, BLACK = 1;
 	Intervals lineCollection;
 	RBTree Tree;
 	
 	@Before
-	public void testInitial() {
+	public void testInit() {
 		lineCollection = new Intervals();
 		Tree = lineCollection.getRBTree();
 	}
 
 	@Test
 	public void startingTest() {
+		testInit();
 		assertEquals(Tree.getRoot(), Tree.getNILNode());
+		assertEquals(Tree.getRoot().parent, Tree.getNILNode());
+		assertEquals(Tree.getRoot().right, Tree.getNILNode());
+		assertEquals(Tree.getRoot().left, Tree.getNILNode());
+	}
+	
+	/**
+	 * Root should be node one and it should reference Nil
+	 */
+	@Test
+	public void testOneInsertion() {
+		testInit();
+		Node one = new Node(new Endpoint(5, 0, 1), Tree.getNILNode());
+		Tree.insert(one);
+
+		assertEquals(Tree.getRoot(), one);
+		assertEquals(Tree.getRoot().parent, Tree.getNILNode());
 		assertEquals(Tree.getRoot().right, Tree.getNILNode());
 		assertEquals(Tree.getRoot().left, Tree.getNILNode());
 	}
 	
 	@Test
-	public void testOneInsertion() {
+	public void testSecondInsertion() {
+		testOneInsertion();
+		
+		Node two = new Node(new Endpoint(6, 0, -1), Tree.getNILNode());
+		Tree.insert(two);
+
+		assertEquals(Tree.getRoot().right, two);
+		assertEquals(Tree.getRoot().right.parent, Tree.getRoot());
+
+		assertEquals(Tree.getRoot().right.color, RED);
+		assertEquals(Tree.getRoot().color, BLACK);
 		
 	}
-	
 }
